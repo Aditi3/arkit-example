42b0c4dbb00c54c22375db1a0ec0dedd74527179
Aditi3 <aditi.gvc@gmail.com>
2021-06-14 18:17:21 +0530

    Refactor Code for Better Readability

--- Sphere/Sphere/ViewController.swift
+++ Sphere/Sphere/ViewController.swift
@@ -12,8 +12,9 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
     
     @IBOutlet var sceneView: ARSCNView!
-    
     var sphereArray = [SCNNode]()
+    
+    // MARK: - Life Cycle Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,32 +25,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
-        
-    }
-    
-    func setupMoon(x: Float, y: Float, z: Float) {
-        
-        let sphere = SCNSphere(radius: 0.2)
-        
-        let material = SCNMaterial()
-        material.diffuse.contents = UIImage(named:"art.scnassets/earth.jpeg")
-        sphere.materials = [material]
-        
-        let sphereNode = SCNNode()
-        sphereNode.position = SCNVector3Make(x, y + sphereNode.boundingSphere.radius, z)
-        sphereNode.geometry = sphere
-        
-        sphereArray.append(sphereNode)
-        sceneView.scene.rootNode.addChildNode(sphereNode)
-        
-        let randomX = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
-        let randomZ = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
-        
-        sphereNode.runAction(SCNAction.rotateBy(x: CGFloat(randomX * 3),
-                                                y: 0,
-                                                z: CGFloat(randomZ * 3),
-                                                duration: 0.5))
-        sceneView.autoenablesDefaultLighting = true
         
     }
     
@@ -79,19 +54,39 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    
+    // MARK: - Setup Sphere Scene
+    
+    func setupSphere(atLocation location: ARHitTestResult) {
         
-        if let touch = touches.first {
-            
-            let touchLocation = touch.location(in: sceneView)
-            let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
-            
-            if let hitResult = results.first {
-                removeSphere()
-                setupMoon(x: hitResult.worldTransform.columns.3.x, y: hitResult.worldTransform.columns.3.y, z: hitResult.worldTransform.columns.3.z)
-            }
-        }
+        let sphere = SCNSphere(radius: 0.2)
+        
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named:"art.scnassets/moon.jpeg")
+        sphere.materials = [material]
+        
+        let sphereNode = SCNNode()
+        
+        sphereNode.position = SCNVector3Make(location.worldTransform.columns.3.x,
+                                             location.worldTransform.columns.3.y + sphereNode.boundingSphere.radius,
+                                             location.worldTransform.columns.3.z)
+        sphereNode.geometry = sphere
+        
+        sphereArray.append(sphereNode)
+        sceneView.scene.rootNode.addChildNode(sphereNode)
+        
+        let randomX = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
+        let randomZ = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
+        
+        sphereNode.runAction(SCNAction.rotateBy(x: CGFloat(randomX * 3),
+                                                y: 0,
+                                                z: CGFloat(randomZ * 3),
+                                                duration: 0.5))
+        sceneView.autoenablesDefaultLighting = true
+        
     }
+    
+    // MARK: - Remove Sphere Scene
     
     func removeSphere() {
         if !sphereArray.isEmpty {
@@ -101,27 +96,55 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         }
     }
     
+    
+    // MARK: - Touch Began Delegate Method
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if let touch = touches.first {
+            
+            let touchLocation = touch.location(in: sceneView)
+            let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+            
+            if let hitResult = results.first {
+                removeSphere()
+                setupSphere(atLocation: hitResult)
+            }
+        }
+    }
+    
+    
+    // MARK: - ARSCNViewDelegateMethod
+    
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        if anchor is ARPlaneAnchor {
-            
-            let planeAnchor = anchor as! ARPlaneAnchor
-            
-            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
-            
-            let planeNode = SCNNode()
-            planeNode.position = SCNVector3(x: planeAnchor.center.x, y: planeAnchor.center.y, z: planeAnchor.center.z)
-            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
-            
-            let gridMaterial = SCNMaterial()
-            gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.jpeg")
-            
-            plane.materials = [gridMaterial]
-            planeNode.geometry = plane
-            node.addChildNode(planeNode)
-            
-        } else {
+        
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {
             return
         }
+        
+        let planeNode = createPlane(withPlaneAnchor: planeAnchor)
+        
+        node.addChildNode(planeNode)
+        
+    }
+    
+    // MARK: - Plane Rendering Method
+    
+    func createPlane(withPlaneAnchor planeAnchor: ARPlaneAnchor) -> SCNNode {
+        
+        let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+        
+        let planeNode = SCNNode()
+        planeNode.position = SCNVector3(x: planeAnchor.center.x, y: planeAnchor.center.y, z: planeAnchor.center.z)
+        planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+        
+        let gridMaterial = SCNMaterial()
+        gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.jpeg")
+        
+        plane.materials = [gridMaterial]
+        planeNode.geometry = plane
+        
+        return planeNode
     }
     
 }
