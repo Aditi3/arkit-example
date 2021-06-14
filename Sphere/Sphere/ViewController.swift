351f0308bdbccfe1c71394aa176de3d2a3cf4380
Aditi3 <aditi.gvc@gmail.com>
2021-06-14 16:56:33 +0530

    Added Sphere Scene

---
+++ Sphere/Sphere/ViewController.swift
@@ -0,0 +1,96 @@
+//
+//  ViewController.swift
+//  Sphere
+//
+//  Created by Aditi Agrawal on 14/06/21.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+    
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        setupMoon()
+    }
+    
+    func setupMoon() {
+        
+        let sphere = SCNSphere(radius: 0.2)
+        
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named:"art.scnassets/earth.jpeg")
+        sphere.materials = [material]
+        
+        let node = SCNNode()
+        node.position = SCNVector3Make(0, 0, -0.5)
+        node.geometry = sphere
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        sceneView.autoenablesDefaultLighting = true
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        guard ARWorldTrackingConfiguration.isSupported else {
+            print("*** ARConfig: AR World Tracking Not Supported")
+            return
+        }
+        
+        let config = ARWorldTrackingConfiguration()
+        config.worldAlignment = .gravity
+        config.providesAudioData = false
+        config.planeDetection = .horizontal
+        config.isLightEstimationEnabled = true
+        config.environmentTexturing = .automatic
+        // Run the view's session
+        sceneView.session.run(config)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    // MARK: - ARSCNViewDelegate
+    
+    /*
+     // Override to create and configure nodes for anchors added to the view's session.
+     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+     let node = SCNNode()
+     
+     return node
+     }
+     */
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
